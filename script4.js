f5c32574eae458a8a1e81872fabff1fee4380641
Matias LVM <matiasmaldonado.eth@gmail.com>
2024-08-19 19:57:53 -0300

    actualización JS

---
+++ script4.js
@@ -0,0 +1,67 @@
+/* let suma2 = 5 + 3;
+var resta2 = 10 - 4;
+let mul2 = 2 * 6;
+let div2 = 7 / 3;  
+let mod2 = 7 % 3;   
+let incr2 = 3;
+incr2++;
+let entera2 = 30;
+
+console.log(suma2);
+console.log(resta2);
+console.log(mul2);
+console.log(div2);
+console.log(mod2);
+console.log(incr2); */ 
+
+let andlogico = true && false;
+let or = true || false;
+let not = !true; 
+
+let x = false;
+let y = true;
+console.log(x + y);
+
+let nombre  = "juan";
+let apellido = "perez";
+let full = nombre + " " + apellido;
+console.log(full);
+console.log(nombre + 5);
+
+console.log(5 * "hola")
+
+let edad = 10;
+const edadMinima = 18;
+if (edad >= edadMinima) {
+    console.log("permitido")
+}
+else {
+    console.log("no permitido")
+}
+let mayorDeEdad = edad >= edadMinima;
+{/*<button hidden={!mayorDeEdad}></button> */}
+
+function example() {
+    var variable = 15;
+    if (true) {
+        var variable = 150;
+        console.log(variable);
+    }
+    console.log(variable);
+}
+
+function example2() {
+    let variable = 10;
+    if (true) {
+        let variable = 20;
+        console.log(variable);
+    }
+    console.log(variable);
+}
+example();
+example2();
+
+// prompt, alert, confirm son cosas de JS, no hace falta agregarlas externamente
+let userAge = prompt("Ingrese su edad: ");
+console.log({userAge});
+// console.log("Su edad es " + userAge)
